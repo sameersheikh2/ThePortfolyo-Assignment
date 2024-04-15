03b58b52683e264b3dbbd2873288b712577d78bd
sameer sheikh <sameersheikh819@gmail.com>
2024-04-15 19:23:40 +0530

    modify hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 const Hero = ({ about }) => {
     return (
-        <main className="flex w-[85%] mt-24 flex-col-reverse md:flex-row m-auto justify-center items-center">
+        <main className="flex w-[85%] mt-16 flex-col-reverse md:flex-row m-auto justify-center items-center">
             <div className="text-center md:text-left">
                 <h1 className="md:text-3xl text-xl ">My Name is {about?.name}</h1>
-                <h2 className="font-serif font-semibold mb-8 md:text-6xl text-3xl my-10">{`I'm a ${about?.title}`}</h2>
+                <h2 className="font-serif font-semibold mb-8 md:text-6xl lg:text-8xl text-5xl my-10">{`I'm a ${about?.title}`}</h2>
                 <span className=" text-sm md:text-base ">{about?.subTitle}</span>
             </div>
-            <img className="mb-6" src={about?.avatar?.url} width={200} height={300} alt="" />
+            <img className="mb-6" src={about?.avatar?.url} width={300} height={400} alt="" />
         </main>
     );
 };
