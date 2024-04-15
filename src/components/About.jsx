811d0f3849a659c88a81fa0e5cb79bb7f402740b
sameer sheikh <sameersheikh819@gmail.com>
2024-04-15 19:13:43 +0530

    add about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+const About = ({ about }) => {
+    return (
+        <>
+            <div className="w-[85%] m-auto mt-28">
+                <h2 className="text-neutral-400 text-xl tracking-wider mb-8">About Me</h2>
+                <div>
+                    <h3 className="text-neutral-400">{about?.quote}</h3>
+                    <h1 className="text-5xl font-serif font-bold mt-5 mb-10 max-w-[37rem]">{about?.subTitle}</h1>
+                </div>
+                <div>
+                    <p className="">{about?.description}</p>
+                </div>
+            </div>
+        </>
+    );
+};
 
-function About() {
-  return (
-    <div>About</div>
-  )
-}
-
-export default About
+export default About;
